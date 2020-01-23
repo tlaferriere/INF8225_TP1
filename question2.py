c84f0746e9d2fd535c0794856b502c0480dad0d2
Roberto Calvi <calvi.berto.rc@gmail.com>
2020-01-22 23:08:10 -0500

    terrible typo fixed

--- question2.py
+++ question2.py
@@ -4,13 +4,17 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
-digits = datasets.load_digits()
-# ____________________________visualizing digits_______________________________________________
+digits = datasets.load_digits()  # Load digits dataset
+
+# ________________________________________________visualizing digits____________________________________________________
+
 print(digits.data.shape)
 
 plt.gray()
-plt.matshow(digits.images[0])
+plt.matshow(digits.images[0])  # you can
 plt.show
+
+# ____________________________________________________Preparing data____________________________________________________
 
 X = digits.data
 
@@ -27,7 +31,7 @@ X_test, X_validation, y_test, y_validation = train_test_split(X_test, y_test,
 
 W = np.random.normal(0, 0.01, (len(np.unique(y)), X.shape[1]))  # weights of shape KxL
 
-best_W = None
+best_W = None  # TODO: dont think this should be a none
 best_accuracy = 0
 lr = 0.001
 nb_epochs = 50
@@ -38,22 +42,55 @@ accuracies = []
 
 
 def softmax(x):
-    # assurez vous que la fonction est numeriquement stable
-    # e.g. softmax(np.array([1000, 1000, 10000], ndim=2))
-    pass
+    """
+    :param x: Takes in the training data
+    :return: Returns normalized probability distribution consisting of probabilities proportional to the exponentials
+            of the input numbers.
 
+    Note du prof
+            assurez vous que la fonction est numeriquement stable
+            e.g. softmax(np.array([1000, 1000, 10000], ndim=2))
+    """
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+
+
+# ________________________________________________Side Functions________________________________________________________
 
 def get_accuracy(X, y, W):
+    """
+    Definition of accuracy: proportion of examples for which the model produces the correct output.
+
+    :param X: Inputs
+    :param y: Outputs to validate predictions
+    :param W: Weight
+    :return:
+    """
     pass
 
 
 def get_grads(y, y_pred, X):
+    """
+
+    :param y:
+    :param y_pred:
+    :param X:
+    :return:
+    """
     pass
 
 
 def get_loss(y, y_pred):
+    """
+
+    :param y:
+    :param y_pred:
+    :return:
+    """
     pass
 
+
+# ___________________________________________________Resolution_________________________________________________________
 
 for epoch in range(nb_epochs):
     loss = 0
@@ -61,10 +98,12 @@ for epoch in range(nb_epochs):
     for i in range(0, X_train.shape[0], minibatch_size):
         pass  # TODO
     losses.append(loss)  # compute the loss on the train set
-    accuracy = None  # TODO
+    accuracy = get_accuracy(X, y, W)  # TODO
     accuracies.append(accuracy)  # compute the accuracy on the validation set
     if accuracy > best_accuracy:
         pass  # select the best parameters based on the validation accuracy
+
+# ___________________________________________________Resolution_________________________________________________________
 
 accuracy_on_unseen_data = get_accuracy(X_test, y_test, best_W)
 print(accuracy_on_unseen_data)  # 0.897506925208
